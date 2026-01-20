9b4b13842369aa192e985194d33c0dbe917e0ef1
Angga Indra Wijaya <anggafxmail2@gmail.com>
2026-01-20 22:33:27 +0700

    change store session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_novena_session'
+Rails.application.config.session_store :cookie_store,key: '_novena_session', same_site: :lax, secure: true
